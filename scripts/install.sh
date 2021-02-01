35341d955285834ac46964a18f10a1b5f396ab75
Mark Lucernas <marklcrns@gmail.com>
2021-02-01 08:31:07 -0800

    refactor(install) Added Android support

--- scripts/install.sh
+++ scripts/install.sh
@@ -58,13 +58,19 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   running "Found you use Linux"
-  if [ -x "$(command -v apk)" ];then sudo apk add bat ripgrep xclip zenity zeal taskwarrior ccls; fi
-  if [ -x "$(command -v pkg)" ];then sudo pkg install bat ripgrep xclip zenity zeal taskwarrior ccls; fi
-  if [ -x "$(command -v pacman)" ];then sudo pacman -S bat ripgrep xclip zenity zeal taskwarrior ccls; fi
-  if [ -x "$(command -v apt)" ]; then sudo apt install bat ripgrep xclip zenity zeal taskwarrior ccls; fi
-  if [ -x "$(command -v dnf)" ]; then sudo dnf install bat ripgrep xclip zenity zeal taskwarrior ccls; fi
-  if [ -x "$(command -v nix-env)" ]; then sudo nix-env -i bat ripgrep xclip zenity zeal taskwarrior ccls; fi
-  if [ -x "$(command -v zypper)" ]; then sudo zypper install bat ripgrep xclip zenity zeal taskwarrior ccls; fi
+
+	if [ "$(uname -o)" == "Android" ]; then
+		# Android Termux
+		if [ -x "$(command -v pkg)" ];then pkg install bat ripgrep taskwarrior; fi
+	else
+		if [ -x "$(command -v apk)" ];then sudo apk add bat ripgrep xclip zenity zeal taskwarrior ccls; fi
+		if [ -x "$(command -v pkg)" ];then sudo pkg install bat ripgrep xclip zenity zeal taskwarrior ccls; fi
+		if [ -x "$(command -v pacman)" ];then sudo pacman -S bat ripgrep xclip zenity zeal taskwarrior ccls; fi
+		if [ -x "$(command -v apt)" ]; then sudo apt install bat ripgrep xclip zenity zeal taskwarrior ccls; fi
+		if [ -x "$(command -v dnf)" ]; then sudo dnf install bat ripgrep xclip zenity zeal taskwarrior ccls; fi
+		if [ -x "$(command -v nix-env)" ]; then sudo nix-env -i bat ripgrep xclip zenity zeal taskwarrior ccls; fi
+		if [ -x "$(command -v zypper)" ]; then sudo zypper install bat ripgrep xclip zenity zeal taskwarrior ccls; fi
+	fi
 fi
 
 ok "\n
