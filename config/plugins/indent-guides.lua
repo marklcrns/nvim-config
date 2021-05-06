adfbe32eb91c21d2e5570235439b362abe2bea93
Mark Lucernas <marklcrns@gmail.com>
2021-05-05 22:49:15 -0700

    refactor(plugins): Update indent-guides config

--- config/plugins/indent-guides.lua
+++ config/plugins/indent-guides.lua
@@ -4,7 +4,7 @@ indent_guide_size = 1;
 indent_start_level = 1;
 indent_enable = true;
 indent_space_guides = true;
-indent_tab_guides = false;
+indent_tab_guides = true;
 indent_soft_pattern = '\\s';
 exclude_filetypes = {'help','dashboard','dashpreview','NvimTree','vista','sagahover'};
 even_colors = { fg='#2a3834',bg='#332b36' };
@@ -16,6 +16,6 @@ require('indent_guides').setup({
     'vista', 'vista_kind', 'tagbar', 'lsp-hover', 'clap_input', 'fzf',
     'any-jump', 'gina-status', 'gina-commit', 'gina-log', 'minimap',
     'quickpick-filter', 'lsp-quickpick-filter', 'calendar', 'coc-explorer',
-    'dashboard', 'codi', 'which_key', 'taskreport'
+    'dashboard', 'codi', 'which_key', 'taskreport', 'floaterm'
   };
 })
