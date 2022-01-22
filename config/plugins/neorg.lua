d5c12821e9f3a3d971bbb009429592149ac4281a
Mark Lucernas <marklcrns@gmail.com>
2022-01-21 20:09:54 -0800

    feat(plugins) Add neorg

---
+++ config/plugins/neorg.lua
@@ -0,0 +1,47 @@
+require('neorg').setup {
+  -- Tell Neorg what modules to load
+  load = {
+    ["core.defaults"] = {}, -- Load all the default modules
+    ["core.keybinds"] = { -- Configure core.keybinds
+      config = {
+        default_keybinds = true, -- Generate the default keybinds
+        neorg_leader = "<LocalLeader>n" -- This is the default if unspecified
+      }
+    },
+    ["core.norg.concealer"] = {}, -- Allows for use of icons
+    ["core.norg.dirman"] = { -- Manage your directories with Neorg
+      config = {
+        workspaces = {
+          my_workspace = "~/neorg"
+        }
+      }
+    }
+  },
+}
+
+
+local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_configs.norg = {
+  install_info = {
+    url = "https://github.com/nvim-neorg/tree-sitter-norg",
+    files = { "src/parser.c", "src/scanner.cc" },
+    branch = "main"
+  },
+}
+
+parser_configs.norg_meta = {
+  install_info = {
+    url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+    files = { "src/parser.c" },
+    branch = "main"
+  },
+}
+
+parser_configs.norg_table = {
+  install_info = {
+    url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+    files = { "src/parser.c" },
+    branch = "main"
+  },
+}
