8929754a8925835f897b566d03c5db36f3e9e95c
Mark Lucernas <marklcrns@gmail.com>
2022-08-23 05:11:38 -0700

    refactor(plugins): Update dashboard

--- config/plugins/dashboard.lua
+++ config/plugins/dashboard.lua
@@ -62,3 +62,17 @@ db.custom_center = {
   },
 }
 
+
+vim.cmd([[
+  augroup dashboard_enter
+    au!
+    autocmd FileType dashboard noremap <nowait><silent><buffer> e :enew<CR>
+    autocmd FileType dashboard noremap <nowait><silent><buffer> f :Telescope find_files<CR>
+    autocmd FileType dashboard noremap <nowait><silent><buffer> h :Telescope oldfiles<CR>
+    autocmd FileType dashboard noremap <nowait><silent><buffer> m :Telescope marks<CR>
+    autocmd FileType dashboard noremap <nowait><silent><buffer> s :<C-u>call feedkeys(':SessionLoad<Space><Tab>','t')<CR>
+    autocmd FileType dashboard noremap <nowait><silent><buffer> t :call DToday()<CR>
+    autocmd FileType dashboard noremap <nowait><silent><buffer> W :VimwikiUISelect<CR>
+    autocmd FileType dashboard noremap <nowait><silent><buffer> n :Neorg workspace<CR>
+  augroup END
+]])
