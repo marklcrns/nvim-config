62a11862cb9e5a8bdce415998cddbba0a86df5b8
Mark Lucernas <marklcrns@gmail.com>
2022-09-28 17:35:16 -0700

    refactor(plugins): Enable cmp-tabnine back

--- config/plugins/nvim-cmp.lua
+++ config/plugins/nvim-cmp.lua
@@ -222,7 +222,7 @@ cmp.setup({
     --   },
     --   group_index = 1,
     -- },
-    -- { name = 'cmp_tabnine', group_index = 1 },
+    { name = 'cmp_tabnine', group_index = 1 },
     { name = 'nvim_lsp', group_index = 1 },
     { name = 'nvim_lua', group_index = 1 },
     { name = 'ultisnips', group_index = 1, max_item_count = 10 },
--- config/plugins/vimtex.vim
+++ config/plugins/vimtex.vim
@@ -13,7 +13,6 @@ let g:vimtex_view_method = 'zathura'
 let g:vimtex_mappings_enabled = 1
 let g:matchup_override_vimtex = 1
 let g:matchup_matchparen_deferred = 1
-let g:vimtex_quickfix_mode = 0
 
 set conceallevel=1
 let g:tex_conceal='abdmg'
