6c1b4935139d9608e413216c42069eb2d4e13f37
Mark Lucernas <marklcrns@gmail.com>
2022-10-16 00:08:36 -0700

    refactor(plugins): Update configs

--- config/plugins/nvim-ts-autotag.lua
+++ config/plugins/nvim-ts-autotag.lua
@@ -6,7 +6,6 @@ autotag.setup {
     'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
     'xml',
     'php',
-    'markdown',
     'glimmer', 'handlebars', 'hbs'
   },
 }
