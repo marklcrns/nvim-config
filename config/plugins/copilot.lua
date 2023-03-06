02dafdc77249594175fa17578719e29cc1034156
Mark Lucernas <marklcrns@gmail.com>
2023-03-06 10:11:40 -0800

    fix(plugins): copilot

--- config/plugins/copilot.lua
+++ config/plugins/copilot.lua
@@ -3,5 +3,17 @@ vim.defer_fn(function()
   require("copilot").setup({
     suggestion = { enabled = false },
     panel = { enabled = false },
+    filetypes = {
+      yaml = false,
+      -- markdown = false,
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ["."] = false,
+      ["*"] = true, -- for all other filetypes
+    },
   })
 end, 100)
--- config/plugins/nvim-cmp.lua
+++ config/plugins/nvim-cmp.lua
@@ -200,7 +200,7 @@ cmp.setup({
       },
       group_index = 1,
     },
-    { name = "cmp_tabnine", group_index = 1 },
+    -- { name = "cmp_tabnine", group_index = 1 },
     { name = "nvim_lsp", group_index = 1 },
     { name = "nvim_lua", group_index = 1 },
     { name = "ultisnips", group_index = 1, max_item_count = 10 },
@@ -309,7 +309,7 @@ cmp.setup.filetype({ "gitcommit", "gina-commit", "NeogitCommitMessage" }, {
 cmp.setup.filetype({ "markdown", "vimwiki", "norg", "help", "text" }, {
   sources = cmp.config.sources({
     { name = "copilot", group_index = 1 },
-    { name = "cmp_tabnine", group_index = 1 },
+    -- { name = "cmp_tabnine", group_index = 1 },
     { name = "nvim_lsp", group_index = 1 },
     -- { name = 'spell', group_index = 1  },
     { name = "buffer", group_index = 2 },
