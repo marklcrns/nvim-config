e3b43f377f9ecfce6964d61d593ce1570407353e
Mark Lucernas <marklcrns@gmail.com>
2023-04-22 21:54:26 -0700

    refactor(plugins): Update indent-o-matic config

--- lua/user/plugins/indent-o-matic.lua
+++ lua/user/plugins/indent-o-matic.lua
@@ -3,8 +3,17 @@ return function()
     -- Number of lines without indentation before giving up (-1 for infinite)
     max_lines = 2048,
     -- Space indentations that should be detected
-    standard_widths = { 2, 3, 4, 8 },
+    standard_widths = { 2, 4, 8 },
     -- Skip multi-line comments and strings (more accurate detection but less performant)
     skip_multiline = true,
+
+    filetype_markdown = {
+      standard_widths = { 2 },
+    },
+
+    -- Don't detect 8 spaces indentations inside files without a filetype
+    filetype_ = {
+      standard_widths = { 2, 4 },
+    },
   })
 end
