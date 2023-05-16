ac47d299017860d9694edcd4511478d5c9c128e6
Mark Lucernas <marklcrns@gmail.com>
2023-05-15 22:59:19 -0700

    refactor(plugins): Update nvim-cmp mappings

--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -279,7 +279,6 @@ return function()
       ["<CR>"] = cmp.mapping({
         i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
         s = cmp.mapping.confirm({ select = true }),
-        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
       }),
     },
   })
