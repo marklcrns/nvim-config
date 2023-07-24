e41d315a4be89772e470c58dab64b5720bca5ab6
Mark Lucernas <marklcrns@gmail.com>
2023-07-24 10:21:46 -0700

    refactor(plugins): update tint

--- lua/user/plugins/tint.lua
+++ lua/user/plugins/tint.lua
@@ -1,7 +1,7 @@
 return function()
   require("tint").setup({
     transforms = {
-      require("tint.transforms").tint_with_threshold(-100, "#1C1C1C", 150), -- Try to tint by `-100`, but keep all colors at least `150` away from `#1C1C1C`
+      require("tint.transforms").tint_with_threshold(-100, "#1C1C1C", 200), -- Try to tint by `-100`, but keep all colors at least `150` away from `#1C1C1C`
       require("tint.transforms").saturate(0.5),
     },
   })
