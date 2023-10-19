86a94e3526dcb0870a20265f4188d95f89244aaa
Mark Lucernas <marklcrns@gmail.com>
2023-10-19 08:29:02 -0700

    fix(lsp): signature

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -42,13 +42,15 @@ require("lspconfig.ui.windows").default_options.border = "single"
 
 ---@diagnostic disable-next-line: unused-local
 function M.common_on_attach(client, bufnr)
-  -- require("illuminate").on_attach(client)
-  require("lsp_signature").on_attach({
-    bind = true, -- This is mandatory, otherwise border config won't get registered.
-    handler_opts = {
-      border = "single",
-    },
-  }, bufnr)
+  if not vim.g.low_performance_mode then
+    require("illuminate").on_attach(client)
+    require("lsp_signature").on_attach({
+      bind = true, -- This is mandatory, otherwise border config won't get registered.
+      handler_opts = {
+        border = "single",
+      },
+    }, bufnr)
+  end
 end
 
 M.base_config = {
