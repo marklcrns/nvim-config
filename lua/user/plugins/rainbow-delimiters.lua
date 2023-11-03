4bc712baee0346e13d9381c1fc93d27832b31d4d
Mark Lucernas <marklcrns@gmail.com>
2023-11-03 14:38:10 -0700

    fix(plugins): rainbow_delimiters plugin

--- lua/user/plugins/rainbow-delimiters.lua
+++ lua/user/plugins/rainbow-delimiters.lua
@@ -1,7 +1,7 @@
 return function()
   local rainbow_delimiters = require("rainbow-delimiters")
 
-  require("rainbow-delimiters.setup")({
+  require("rainbow-delimiters.setup").setup({
     strategy = {
       [""] = rainbow_delimiters.strategy["global"],
     },
