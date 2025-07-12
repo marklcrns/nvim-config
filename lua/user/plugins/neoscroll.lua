25a61f227cd7a141239fa122528315832153d0ae
Mark Lucernas <mrklcrns@amazon.com>
2025-07-12 08:19:59 -0700

    refactor(plugins): update

--- lua/user/plugins/neoscroll.lua
+++ lua/user/plugins/neoscroll.lua
@@ -44,11 +44,11 @@ return function()
 
   local keymap = {
     -- Use the "sine" easing function
-    ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 100; easing = 'sine' }) end;
-    ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 100; easing = 'sine' }) end;
+    ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 70; easing = 'sine' }) end;
+    ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 70; easing = 'sine' }) end;
     -- Use the "circular" easing function
-    ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 200; easing = 'circular' }) end;
-    ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 200; easing = 'circular' }) end;
+    ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 100; easing = 'circular' }) end;
+    ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 100; easing = 'circular' }) end;
     -- When no value is passed the `easing` option supplied in `setup()` is used
     ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 50 }) end;
     ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 50 }) end;
